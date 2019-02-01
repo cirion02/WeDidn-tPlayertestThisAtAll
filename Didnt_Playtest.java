744a8ca84bb10ad1065059672ee2131f2a1ae0ad
cirion02 <34488326+cirion02@users.noreply.github.com>
2019-02-01 13:29:16 +0100

    Update Didnt_Playtest.java

--- Didnt_Playtest.java
+++ Didnt_Playtest.java
@@ -161,6 +161,20 @@ public class DidntPlaytest extends JFrame {
 		}
 	}
 	
+	public class cardPoints implements playable {
+		static final String name = "Points";
+		static final String text = "You get +8 points, anyone with 15 or more points wins.";
+		public void playCard(int Player) {
+			Points(Player);
+		}
+		public String getName() {
+			return name;
+		}
+		public String getText() {
+			return text;
+		}
+	}
+	
 	/* Make an Object for each class */
 	cardPc kaartPc = new cardPc();
 	cardILose kaartILose = new cardILose();
@@ -170,6 +184,7 @@ public class DidntPlaytest extends JFrame {
 	cardBattleScissors kaartBattleScissors = new cardBattleScissors();
 	cardNumbersOdd kaartNumbersOdd = new cardNumbersOdd();
 	cardNumbersEven kaartNumbersEven = new cardNumbersEven();
+	cardPoints kaartPoints = new cardPoints();
 	
 	/* Variablelen aanmaken */
 	 ArrayList<playable> player1Hand=new ArrayList<playable>();
@@ -177,7 +192,12 @@ public class DidntPlaytest extends JFrame {
 	 ArrayList<playable> library=new ArrayList<playable>(); 
 	 ArrayList<String> Prompts = new ArrayList<String>();
 	 ArrayList<playable> cards = new ArrayList<playable>();
+	 ArrayList<playable> totalBattlefield = new ArrayList<playable>();
+	 ArrayList<playable> player1Battlefield = new ArrayList<playable>();
+	 ArrayList<playable> player2Battlefield = new ArrayList<playable>();
 	 String returnFunction = "";
+	 int player1points = 0;
+	 int player2points = 0;
 		
 	/* Alles objecten predifineren */
 	JLabel l1 = new JLabel("");
@@ -254,6 +274,21 @@ public class DidntPlaytest extends JFrame {
 		
 	}
 	
+	public void Points(int Player) {
+		switch (Player) {
+		case 1 :	player1points += 8;
+					player1Battlefield.add(kaartPoints);
+					if (player1points > 14) {
+						playerWins(1);
+					}
+		case 2 :	player2points += 8;
+					player2Battlefield.add(kaartPoints);
+					if (player2points > 14) {
+						playerWins(2);
+					}
+		}
+	}
+	
 	/* the function the confirm button calls for Battle! cards */
 	public void RockPaperScissorsDone(String Loses, String Picked) {
 		if (Loses == Picked) {
@@ -305,69 +340,30 @@ public class DidntPlaytest extends JFrame {
 	/* Makes a player wins the game */
 	public void playerWins(int player) {
 		l1.setText(l1.getText() + player + " has won the game.  ");
-		System.exit(0);
 	}
 	
 	/* calls all functions that have to happen at the start of the game */
 	public void startOfGame(){
 		fillLibrary(10);
 		fillHand();
-		int startingPlayer = (int) Math.random() + 1;
-		if (startingPlayer == 1) {
-			
-		}
-		if (startingPlayer == 2) {
-			AIPlaysCard();
-		}
 	}
 	
 	/* If you give this a cardname it will activate that cards effect */
 	public void runCard(String Name, int Player) {
-		if (Player == 1) {
-			b0.setEnabled(false);
-			b1.setEnabled(false);
-			b2.setEnabled(false);
-			b3.setEnabled(false);
-			b4.setEnabled(false);
-		}
+		
 		for (int i=0; i<cards.size(); i++) {
 			playable testObject = cards.get(i);
 			String name = testObject.getName();
 			if (Name == name) {
 				testObject.playCard(Player);
-				kaartGespeeld = false;
 			}
 		}
 	}
 	
 	/* If you give this a card-name it will activate that cards effect */
-	public void AIPlaysCard() {
-		b0.setEnabled(false);
-		b1.setEnabled(false);
-		b2.setEnabled(false);
-		b3.setEnabled(false);
-		b4.setEnabled(false); 
-		int random = (int) Math.random() * player2Hand.size();
-		runCard(player2Hand.get(random).getName(), 2);
-		player2Hand.remove(random);
-		b0.setEnabled(true);
-		b1.setEnabled(true);
-		b2.setEnabled(true);
-		b3.setEnabled(true);
-		b4.setEnabled(true);
-	}
+	
 	/* turnsysteem */
-	public void PlayerTurn() {
-		while (kaartGespeeld = false) {
-		}
-		AITurn();
-	}
-	public void AITurn() {
-		AIPlaysCard();
-		while (kaartGespeeld = false) {
-			
-		}
-	}
+	
 	
 	/*Front End (Mostly) */
 	public DidntPlaytest() {
@@ -380,6 +376,7 @@ public class DidntPlaytest extends JFrame {
 		cards.add(kaartBattleScissors);
 		cards.add(kaartNumbersEven);
 		cards.add(kaartNumbersOdd);
+		cards.add(kaartPoints);
 		startOfGame();
 		Draw(1, 5);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -499,11 +496,6 @@ public class DidntPlaytest extends JFrame {
 		contentPane.add(b2);
 		contentPane.add(b3);
 		contentPane.add(b4);
-		b0.setEnabled(false);
-		b1.setEnabled(false);
-		b2.setEnabled(false);
-		b3.setEnabled(false);
-		b4.setEnabled(false);
 		contentPane.add(Display);
 		contentPane.add(prompt);
 		contentPane.add(confirm);
